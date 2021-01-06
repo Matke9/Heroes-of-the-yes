b9db367e5ea3819de8d67dc76ea6383086f9f147
Matke9 <64710388+Matke9@users.noreply.github.com>
2021-01-06 22:32:09 +0100

    grid

---
+++ Assets/Scripts/Classes/Grid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    private float width;
+    private float height;
+    private float cellSize;
+    private int[,] Cells;
+    public Grid(int width, int height, float cellSize) 
+    {
+        this.width = width;
+        this.height = height;
+        Cells = new int[width, height];
+        this.cellSize = cellSize;
+
+        for (float i = 0; i < Cells.GetLength(0); i++)
+        {
+            for (float j = 0; j < Cells.GetLength(1); j++)
+            {
+                Debug.DrawLine(GetWorldPosition(i, j), GetWorldPosition(i, j + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(i, j), GetWorldPosition(i + 1, j), Color.white, 100f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+    }
+
+    public Vector3 GetWorldPosition(float x, float y)
+    {
+        return new Vector3(x, y) * cellSize - new Vector3(width, height) * cellSize / 2;
+    }
+}
---
+++ Assets/Scripts/GridObject.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Grid grid = new Grid(50,30,0.32f);
+    }
+}
